06ac137eea7d1c9ba733f6954100b024084791a2
caitlin <cm1515@desales.edu>
2022-10-20 10:35:25 -0400

    Secret Update

---
+++ app/src/main/java/com/hfad/secretmessage/EncryptFragment.java
@@ -0,0 +1,29 @@
+package com.hfad.secretmessage;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class EncryptFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_encrypt, container, false);
+        String message = EncryptFragmentArgs.fromBundle(requireArguments()).getMessage();
+        String reversed = "";
+        for (int i = message.length() - 1; i >= 0; i--)
+            reversed += message.charAt(i);
+        TextView tvEncMessage = view.findViewById(R.id.encrypted_message);
+        tvEncMessage.setText(reversed);
+        return view;
+    }
+}
--- app/src/main/java/com/hfad/secretmessage/MessageFragment.java
+++ app/src/main/java/com/hfad/secretmessage/MessageFragment.java
@@ -3,10 +3,13 @@ package com.hfad.secretmessage;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MessageFragment extends Fragment {
@@ -16,6 +19,25 @@ public class MessageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_message, container, false);
+        View view = inflater.inflate(R.layout.fragment_message, container, false);
+        Button btnNext = view.findViewById(R.id.next);
+        TextView messageView = view.findViewById(R.id.etv_message);
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View view) {
+
+                String msg = messageView.getText().toString();
+
+                MessageFragmentDirections.ActionMessageFragmentToEncryptFragment action =
+                        MessageFragmentDirections.actionMessageFragmentToEncryptFragment(msg);
+
+                Navigation.findNavController(view).navigate(action);
+            }
+        });
+        return view;
+
+
     }
 }
